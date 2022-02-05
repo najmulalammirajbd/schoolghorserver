734689c74ec9e964e1d1c4b3bf741ea287f7425d
Najmul Alam Miraj <najmulalammiraj@gmail.com>
2022-02-05 14:29:41 +0600

    m

--- index.js
+++ index.js
@@ -5,34 +5,96 @@ const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config()
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-console.log('uri', uri);
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tfgke.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express()
 
 app.use(bodyParser.json());
 app.use(cors());
 
-const port = 5000;
+const port = 5000
 
-app.get('/', (req, res) => {
-    res.send('hello world');
-})
+
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
-    console.log('err khaise mongodb connection', err);
     const BDSCHOOLGHOR = client.db("SCHOOLGHOR").collection("Regi");
 
     app.post('/regi', (req, res) => {
-        const products = req.body;
-        BDSCHOOLGHOR.insertOne(regi)
+        const users = req.body;
+        BDSCHOOLGHOR.insertOne(users)
+            .then(result => {
+                res.send(result)
+            })
+    })
+    const SCHOOLGHORCON = client.db("SCHOOLGHOR").collection("Con");
+
+    app.post('/con', (req, res) => {
+        const users = req.body;
+        SCHOOLGHORCON.insertOne(users)
             .then(result => {
                 res.send(result)
             })
     })
 
+    const SCHOOLGHORREMO = client.db("SCHOOLGHOR").collection("Remo");
+
+    app.post('/remo', (req, res) => {
+        const users = req.body;
+        SCHOOLGHORREMO.insertOne(users)
+            .then(result => {
+                res.send(result)
+            })
+    })
+
+    const SCHOOLGHORRENU = client.db("SCHOOLGHOR").collection("Renu");
+
+    app.post('/renu', (req, res) => {
+        const users = req.body;
+        SCHOOLGHORRENU.insertOne(users)
+            .then(result => {
+                res.send(result)
+            })
+    })
+
+    const SCHOOLGHORUPDA = client.db("SCHOOLGHOR").collection("Upda");
+
+    app.post('/upda', (req, res) => {
+        const users = req.body;
+        SCHOOLGHORUPDA.insertOne(users)
+            .then(result => {
+                res.send(result)
+            })
+    })
+
+    app.get("/userdata", (req, res) => {
+        IslavoUsers.find({})
+            .toArray((err, document) => {
+                res.send(document)
+            })
+    })
 });
 
+
+// client.connect(err => {
+//     const users = client.db("ISLAVO").collection("service");
+//     console.log(err);
+//     app.post('/users', (req, res) => {
+//         const users = req.body;
+//         users.insertOne(users)
+//             .then(result => {
+//                 res.send(result)
+//             })
+//     })
+// });
+
+
+// app.listen(port, () => {
+//     console.log(`Example app listening at http://localhost:${port}`)
+// })
+
+app.get('/', (req, res) => {
+    res.send('SCHOOL GHOR')
+})
 
 app.listen(process.env.PORT || port)
